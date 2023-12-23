7cff621df34dc5215764d014b3dc4c3eee2fb482
Breno009 <147253894+Breno009@users.noreply.github.com>
2023-12-23 20:26:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var botão = document.querySelector("button")
+var texto = document.querySelector("input")
+botão.addEventListener('click', function(){
+    alert("Obrigado pela sua sujestão ou reclamação! Vamos fazer o possível para deixar o site no agrado de todos");
+    }
+    );
